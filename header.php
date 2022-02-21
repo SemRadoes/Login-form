dfc93aafb72263cbe5da911153dbf1d57f1ed5aa
Aws3m <semradijs@hotmail.com>
2022-02-21 22:08:15 +0100

    added header

---
+++ header.php
@@ -0,0 +1,14 @@
+<?<php>
+<nav>
+        <div class="wrapper">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="discover.php">about us</a></li>
+                <li><a href="blog.php">find blogs</a></li>
+                <li><a href="signup.php">sign up</a></li>
+                <li><a href="login.php">log in</a></li>
+            </ul>
+        </div>
+</nav>
+<div class="wrapper">
+</php>
