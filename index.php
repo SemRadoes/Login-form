eed65092a28668371055d49d994905bd47dbc994
Aws3m <semradijs@hotmail.com>
2022-02-21 21:55:52 +0100

    added header.. need to move header stuff in here

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>login form</title>
+</head>
+<body>
+    
+    <div class="wrapper">
+        <section class="index-intro">
+            <h1>This is an introduction</h1>
+            <p>important paragraph</p>
+        </section>
+        <section class="index-categories">
+            <h2>basic categs</h2>
+            <div class="index-categories-list">
+                <div>
+                    <h3>Stuff</h3>
+                </div>
+                <div>
+                    <h3>Stuff</h3>
+                </div>
+                <div>
+                    <h3>Stuff</h3>
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
+</html>
